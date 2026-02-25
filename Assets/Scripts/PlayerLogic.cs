ae8d99c0ba43a69c77d77214ac3f5c884fadba3c
DeceptiKHAAAAN <lukejronquillo@gmail.com>
2026-02-25 14:41:17 -0600

    Implemented Boundary Walls and Wall Collision Logic

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -3,20 +3,22 @@ using UnityEngine;
 public class PlayerLogic : MonoBehaviour
 {
     [SerializeField] GameObject playerLaser;
-    [SerializeField] int moveSpeed = 1;
+    [SerializeField] float moveSpeed = 1;
+    float finalMoveSpeed;
     float timeSinceLastShot = 0;
     [SerializeField] float timeBetweenShots = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start() {}
+    void Start() 
+    {
+        finalMoveSpeed = moveSpeed;
+    }
 
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Translate(Vector3.right * horizontalInput * moveSpeed * Time.deltaTime);
-
-
+        transform.Translate(Vector3.right * horizontalInput * finalMoveSpeed * Time.deltaTime);
 
         float shootInput = Input.GetAxis("Fire1");
         if (shootInput != 0 && timeSinceLastShot > timeBetweenShots)
@@ -29,5 +31,15 @@ public class PlayerLogic : MonoBehaviour
         timeSinceLastShot = 0;
         Vector3 position = transform.position;
         Instantiate(playerLaser, position, Quaternion.identity);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        finalMoveSpeed = 1;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        finalMoveSpeed = moveSpeed;
     }
 }
