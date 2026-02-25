9b53d5144f4cd06a3a984f512fc5d1e57664cbc3
DeceptiKHAAAAN <lukejronquillo@gmail.com>
2026-02-24 19:14:30 -0600

    Player Ship Created and Player Horizontal Movement Implemented

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] int moveSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * horizontalInput * moveSpeed * Time.deltaTime);
+    }
+}
