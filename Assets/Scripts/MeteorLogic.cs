c44ec0ddcbe5b99860867c990ff783f2f110635e
DeceptiKHAAAAN <lukejronquillo@gmail.com>
2026-02-25 16:32:22 -0600

    Implemented Randomized Rotation to Meteors
    Any meteors spawned will have a random rotation direction and speed. Rotation does not effect that the meteor is moving downwards towards the bottom of the screen.

--- Assets/Scripts/MeteorLogic.cs
+++ Assets/Scripts/MeteorLogic.cs
@@ -3,9 +3,19 @@ using UnityEngine;
 public class MeteorLogic : MonoBehaviour
 {
     [SerializeField] float meteorSpeed;
+    private float degreesPerSecond;
+    private float rotateDirection;
+
+    private void Start()
+    {
+        int randomNumber = Random.Range(0, 2);
+        rotateDirection = randomNumber == 0 ? 1 : -1;
+        degreesPerSecond = Random.Range(10, 70);
+    }
     private void Update()
     {
-        transform.Translate(Vector3.down * meteorSpeed * Time.deltaTime);
+        transform.Translate(Vector3.down * meteorSpeed * Time.deltaTime, Space.World);
+        transform.Rotate(new Vector3(0, 0, degreesPerSecond) * Time.deltaTime * rotateDirection);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
